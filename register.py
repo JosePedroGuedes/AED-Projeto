9815a7a4fbcc2258423cc02438c0c30df0a6542f
Tajaky14 <josepedroguedes2112@gmail.com>
2024-01-07 13:17:19 +0000

    user accounts bugs fixed

--- register.py
+++ register.py
@@ -1,20 +1,27 @@
 import tkinter
-import os
 from tkinter import messagebox
-
 
 janela = tkinter.Tk()
 janela.geometry("500x300")
 janela.title("My Photos Login")
 janela.config(bg="white")
 
-file = open("accounts.txt", "w")
-file.writ
-
-
 def button_save():
+    username_info = username.get()
+    email_info = email_entry.get()
+    password_info = password_entry.get()
+
+    # Verificar se algum dos campos está vazio
+    if not (username_info and email_info and password_info):
+        messagebox.showwarning("Empty Fields", "Please fill in all the fields.")
+        return
+
+    # Escrever as informações no arquivo
+    with open("accounts.txt", "a") as f:
+        f.write(f"Username: {username_info}\nEmail: {email_info}\nPassword: {password_info}\n\n")
+
     print("Account created")
-    res= messagebox.showinfo(title="", message="Your account have been successfully created.")
+    messagebox.showinfo("", "Your account has been successfully created.")
 
 
 def limpar_username(event):
@@ -29,8 +36,8 @@ def limpar_email(event):
 
 def limpar_password(event):
     if password_entry.get() == "Your Password":
-        password_entry.delete(0, tkinter.END)   
-        password_entry.config(show="*")     
+        password_entry.delete(0, tkinter.END)
+        password_entry.config(show="*")
 
 
 title = tkinter.Label(janela, text='My Photos', font=('Arial', 18))
@@ -45,7 +52,7 @@ lblUsername = tkinter.Label(janela, text="Username:")
 lblUsername.place(x=111, y=100)
 lblUsername.config(bg='white')
 
-username = tkinter.Entry(janela,)
+username = tkinter.Entry(janela)
 username.insert(0, "Your Name")
 username.pack(padx=10, pady=10)
 username.bind("<FocusIn>", limpar_username)
@@ -57,7 +64,7 @@ lblEmail.config(bg='white')
 email_entry = tkinter.Entry(janela)
 email_entry.insert(0, "Your Email")
 email_entry.pack(padx=10, pady=10)
-email_entry.bind("<FocusIn>", limpar_email)  
+email_entry.bind("<FocusIn>", limpar_email)
 
 lblPassword = tkinter.Label(janela, text="Password:")
 lblPassword.place(x=113, y=180)
@@ -66,7 +73,7 @@ lblPassword.config(bg='white')
 password_entry = tkinter.Entry(janela)
 password_entry.insert(0, "Your Password")
 password_entry.pack(padx=10, pady=10)
-password_entry.bind("<FocusIn>", limpar_password) 
+password_entry.bind("<FocusIn>", limpar_password)
 
 button_save = tkinter.Button(janela, text="Save", command=button_save)
 button_save.pack(padx=4, pady=10)
