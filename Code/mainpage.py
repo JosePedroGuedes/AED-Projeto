48d26ecefbc296907f0ddd74413d62de268e331b
Jose Guedes <84146957+JosePedroGuedes@users.noreply.github.com>
2024-01-10 15:16:59 +0000

    Fix bugs

---
+++ Code/mainpage.py
@@ -0,0 +1,10 @@
+import tkinter
+
+janela = tkinter.TK()
+janela.geometry = ("1920x1080")
+janela.title = ("My Photos")
+janela.config (bg="white")
+
+
+
+janela.mainloop()
--- Code/register.py
+++ Code/register.py
@@ -36,16 +36,6 @@ def button_save():
     os.system(f'python "{caminho_arquivo2}"')
 
 
-def limpar_username(event):
-    if username.get() == "Your Name":
-        username.delete(0, tkinter.END)
-
-
-def limpar_email(event):
-    if email_entry.get() == "Your Email":
-        email_entry.delete(0, tkinter.END)
-
-
 def limpar_password(event):
     if password_entry.get() == "Your Password":
         password_entry.delete(0, tkinter.END)
@@ -66,7 +56,6 @@ lblUsername.config(bg='white')
 
 username = tkinter.Entry(janela)
 username.pack(padx=10, pady=10)
-username.bind("<FocusIn>", limpar_username)
 
 lblEmail = tkinter.Label(janela, text="Email:")
 lblEmail.place(x=135, y=140)
@@ -74,7 +63,6 @@ lblEmail.config(bg='white')
 
 email_entry = tkinter.Entry(janela)
 email_entry.pack(padx=10, pady=10)
-email_entry.bind("<FocusIn>", limpar_email)
 
 lblPassword = tkinter.Label(janela, text="Password:")
 lblPassword.place(x=113, y=180)
@@ -82,7 +70,6 @@ lblPassword.config(bg='white')
 
 password_entry = tkinter.Entry(janela)
 password_entry.pack(padx=10, pady=10)
-password_entry.bind("<FocusIn>", limpar_password)
 
 button_save = tkinter.Button(janela, text="Save", command=button_save)
 button_save.pack(padx=4, pady=10)
