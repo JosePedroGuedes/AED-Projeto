4f34e9f3c33aad24ff5e2664c00e1b9f872e1333
Edi <eduardo.mb.sousa25@gmail.com>
2024-01-14 17:58:32 +0000

    AED

--- Code/eliminar_accounts.py
+++ Code/eliminar_accounts.py
@@ -3,51 +3,53 @@ from tkinter import ttk
 from tkinter import messagebox
 import os
 
-def eliminar_categoria():
-    
-    categoria_a_eliminar = combo_categoria.get()
-    frase = "Escolha a categoria que deseja eliminar"
+def eliminar_conta():
+    username_a_eliminar = combo_username.get()
+    frase = "Escolha o usuário que deseja eliminar"
 
-    if(categoria_a_eliminar != frase):
-
-        with open('AED-Projeto/Files/categorias.txt', 'r') as f:
+    if username_a_eliminar != frase:
+        with open('AED-Projeto/Files/accounts.txt', 'r') as f:
             linhas = f.readlines()
 
-        linhas = [linha for linha in linhas if linha.strip() != categoria_a_eliminar.strip()]
+        novas_linhas = []
+        capturando_conta = False
+        for linha in linhas:
+            if f"Username: {username_a_eliminar}" in linha:
+                capturando_conta = True
+            if not capturando_conta:
+                novas_linhas.append(linha)
+            if linha.strip() == "":
+                capturando_conta = False
 
-        with open('AED-Projeto/Files/categorias.txt', 'w') as f:
-            f.writelines(linhas)
+        with open('AED-Projeto/Files/accounts.txt', 'w') as f:
+            f.writelines(novas_linhas)
 
         atualizar_combobox()
 
-        mensagem = "A categoria " + categoria_a_eliminar + " foi eliminada com sucesso"
-
+        mensagem = f"A conta do usuário {username_a_eliminar} foi eliminada com sucesso"
         messagebox.showinfo("Mensagem", mensagem)
-
     else:
-        messagebox.showinfo("Mensagem", "Selecione uma categoria")
-    
+        messagebox.showinfo("Mensagem", "Selecione um usuário")
 
 def atualizar_combobox():
-    frase = "Escolha a categoria que deseja eliminar"
-    with open('AED-Projeto/Files/categorias.txt', 'r') as f:
-        categorias = [linha.strip() for linha in f.readlines()]
+    frase = "Escolha o usuário que deseja eliminar"
+    with open('AED-Projeto/Files/accounts.txt', 'r') as f:
+        usuarios = [linha.split(": ")[1].strip() for linha in f.readlines() if "Username: " in linha]
 
-    combo_categoria['values'] = categorias
-    combo_categoria.set(frase)
+    combo_username['values'] = usuarios
+    combo_username.set(frase)
 
     exibir_ficheiro()
 
-
 def exibir_ficheiro():
-    with open('AED-Projeto/Files/categorias.txt', 'r') as f:
+    with open('AED-Projeto/Files/accounts.txt', 'r') as f:
         conteudo = f.read()
+        conteudo = conteudo[:-2]
         print(conteudo)
         caixa_texto.config(state='normal')
         caixa_texto.delete('1.0', tk.END)
         caixa_texto.insert(tk.END, conteudo)
         caixa_texto.config(state='disabled')
-
 
 def voltar():
     janela.destroy()
@@ -55,25 +57,25 @@ def voltar():
     os.system(f'python "{caminho_arquivo}"')
 
 janela = tk.Tk()
-janela.title("Eliminar Categoria")
+janela.title("Eliminar Conta de Usuário")
 
-titulo = tk.Label(janela, text="Escolha a categoria que quer apagar:")
+titulo = tk.Label(janela, text="Escolha o usuário que quer apagar:")
 titulo.pack(padx=20, pady=5)
 
-combo_categoria = ttk.Combobox(janela, state='readonly', width=40)
-combo_categoria.pack(pady=10)
+combo_username = ttk.Combobox(janela, state='readonly', width=40)
+combo_username.pack(pady=10)
 
-categorias = tk.Label(janela, text="Categorias:")
-categorias.pack(padx=20, pady=5)
+usuarios_label = tk.Label(janela, text="Usuários:")
+usuarios_label.pack(padx=20, pady=5)
 
-caixa_texto = tk.Text(janela, wrap=tk.WORD, width=40, height=10, state='disabled')
+caixa_texto = tk.Text(janela, wrap=tk.WORD, width=40, height=15, state='disabled')
 caixa_texto.pack(padx=10, pady=10)
 
-botao_eliminar = tk.Button(janela, text="Eliminar Categoria", command=eliminar_categoria)
+botao_eliminar = tk.Button(janela, text="Eliminar Conta", command=eliminar_conta)
 botao_eliminar.pack(pady=10)
 
-botao_eliminar = tk.Button(janela, text="Voltar", command=voltar)
-botao_eliminar.pack(pady=10)
+botao_voltar = tk.Button(janela, text="Voltar", command=voltar)
+botao_voltar.pack(pady=10)
 
 atualizar_combobox()
 janela.mainloop()
